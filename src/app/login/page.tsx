a1e6e8d2693c47af88ac752c59e47abee357967e
ButiTando <tando.mahlati2907@gmail.com>
2024-01-31 21:54:03 +0200

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,19 +21,21 @@ export default function Login(){
                 <form className="flex flex-col
                                  justify-center
                                  text-white
-                                 w-4/6 h-2/5
+                                 w-4/6 h-[20em]
                                  relative
                                  px-3  
                                  border-[2px] border-white
                                  rounded-2xl"
                       onSubmit={handleSubmit}>
                     <label htmlFor="usernameInput"
-                           className="text-xl
-                                      pl-[5px]">Username</label>
+                           className="text-lg
+                                      pl-[5px]
+                                      h-7 w-4/5">Username</label>
 
                     <input type="text" id="usernameInput"
-                           className="text-2xl
+                           className="text-xl
                                       bg-transparent
+                                      h-10 w-full 
                                       focus:outline-0
                                       border-[2px] border-white
                                       focus:border-[1px] focus:border-gray-400
@@ -44,12 +46,14 @@ export default function Login(){
                                       px-2" />
 
                     <label htmlFor="passwordInput"
-                           className="text-xl
-                                      pl-[5px]">Password</label>
+                           className="text-lg
+                                      pl-[5px]
+                                      h-7 w-4/5">Password</label>
 
                     <input type="text" id="passwordInput"
-                           className="text-2xl
-                                      bg-transparent
+                           className="text-xl
+                                      bg-transparent                                      
+                                      h-10 w-full
                                       focus:outline-0
                                       border-[2px] border-white
                                       focus:border-[1px] focus:border-gray-400
@@ -60,14 +64,13 @@ export default function Login(){
                                       px-2" />
 
                     <button type="submit"
-                            className="text-2xl text-center
+                            className="text-xl text-center
                                        justify-around
                                        bg-slate-800
-                                       w-3/6
-                                       py-1
+                                       w-3/6 h-[2em]                                      py-1
                                        border-[1px] border-white
                                        rounded-2xl">Login</button>
-                    <p className="absolute bottom-1 
+                    <p className="
                                   text-sm
                                   tracking-wide">If you do not have an account, contact your account manager.</p>
                 </form>
